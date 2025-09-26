e19f1550b7154b395558079693d2b75484a6b118
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-09-26 19:23:56 -0300

    Testando rotas

--- auth_routes.py
+++ auth_routes.py
@@ -5,3 +5,19 @@ auth_router = APIRouter(prefix="/auth", tags=["auth"])
 @auth_router.get("/autenticcao")
 async def autenticacao():
     return {"mensagem": "Você acessou a rota de autenticação"}
+
+@auth_router.post("/cadastrar")
+async def pedidos():
+    return{"mensagem": "Cliente cadastrado com sucesso"}
+
+@auth_router.delete("/deletar")
+async def pedidos():
+    return{"mensagem": "Cliente deletado com sucesso"}
+
+@auth_router.put("/atualizar")
+async def pedidos():
+    return{"mensagem": "Cliente atualizado com sucesso"}
+
+@auth_router.patch("/atualizar")
+async def pedidos():
+    return{"mensagem": "Cliente atualizado com sucesso"}
