7fce50fc549f2a3e3266bc556f6b2d1816b579d0
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-09-28 15:18:37 -0300

    Criando tabelas

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine, Colum, String, Integer, Boolean, Float, ForeignKey
 from sqlalchemy.orm import declarative_base
 
-
 db = create_engine("sqlite:///banco.db");
 
 Base = declarative_base();
@@ -10,9 +9,17 @@ class Usuario(Base):
     __tablename__ = "usuarios"
 
     id = Colum("id", Integer, primary_key=True, autoincrement=True);
-    nome = Colum("id", String, nullable=False);
-    email = Colum("id", String, nullable=False);
-    senha = Colum("id", String);
-    ativo = Colum("id", Boolean);
-    admin = Colum("id", Boolean);
+    nome = Colum("nome", String);
+    email = Colum("email", String, nullable=False);
+    senha = Colum("senha", String);
+    ativo = Colum("ativo", Boolean);
+    admin = Colum(" admin", Boolean, default=False);
+
+    def __init__(self, nome, email, senha, ativo = True, admin = False ): 
+        self.nome = nome
+        self.nome = email
+        self.nome = senha
+        self.nome = ativo
+        self.nome = admin
+
 
