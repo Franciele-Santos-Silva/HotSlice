39e975b5f823a8c19d93f178de947e8c96427f45
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-09-28 11:20:15 -0300

    Rotas alteradas

--- auth_routes.py
+++ auth_routes.py
@@ -6,18 +6,3 @@ auth_router = APIRouter(prefix="/auth", tags=["auth"])
 async def autenticacao():
     return {"mensagem": "Você acessou a rota de autenticação"}
 
-@auth_router.post("/cadastrar")
-async def pedidos():
-    return{"mensagem": "Cliente cadastrado com sucesso"}
-
-@auth_router.delete("/deletar")
-async def pedidos():
-    return{"mensagem": "Cliente deletado com sucesso"}
-
-@auth_router.put("/atualizar")
-async def pedidos():
-    return{"mensagem": "Cliente atualizado com sucesso"}
-
-@auth_router.patch("/atualizar")
-async def pedidos():
-    return{"mensagem": "Cliente atualizado com sucesso"}
--- order_routes.py
+++ order_routes.py
@@ -5,15 +5,3 @@ order_router = APIRouter(prefix="/pedidos", tags=["pedidos"])
 @order_router.get("/lista")
 async def pedidos():
     return{"mensagem": "Você acessou a rota de pedidos"}
-
-@order_router.post("/lista")
-async def pedidos():
-    return{"mensagem": "Você acessou a rota de pedidos"}
-
-@order_router.delete("/lista")
-async def pedidos():
-    return{"mensagem": "Você acessou a rota de pedidos"}
-
-@order_router.put("/lista")
-async def pedidos():
-    return{"mensagem": "Você acessou a rota de pedidos"}
