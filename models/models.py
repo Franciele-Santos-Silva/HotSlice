34ff6e8b7fc83d0cdbc383249567493fd687ba40
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-10-10 09:55:34 -0300

    Criando tabela intempedido

--- models/models.py
+++ models/models.py
@@ -14,7 +14,7 @@ nome = Colum("nome", String);
 email = Colum("email", String, nullable=False);
 senha = Colum("senha", String);
 ativo = Colum("ativo", Boolean);
-admin = Colum(" admin", Boolean, default=False);
+admin = Colum("admin", Boolean, default=False);
 
 def __init__(self, nome, email, senha, ativo = True, admin = False ): 
     self.nome = nome
@@ -41,3 +41,8 @@ def __init__(self, usuario, status = "PENDENTE", preco = 0):
     self.status = status
     self.usuario = usuario
     self.preco = preco
+
+
+class ItemPedido(Base):
+    __tablename__ = "pedido_itens"
+
