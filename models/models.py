2dce63438b24ea6996e6bc23b4ebbf53a21b0d62
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-10-10 11:38:37 -0300

    Migration

--- models/models.py
+++ models/models.py
@@ -1,62 +1,60 @@
-from sqlalchemy import create_engine, Colum, String, Integer, Boolean, Float, ForeignKey
+from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float, ForeignKey
 from sqlalchemy.orm import declarative_base
 from sqlalchemy_utils.types import ChoiceType
 
-db = create_engine("sqlite:///banco.db");
+db = create_engine("sqlite:///banco.db")
 
-Base = declarative_base();
+Base = declarative_base()
 
 class Usuario(Base):
     __tablename__ = "usuarios"
 
-id = Colum("id", Integer, primary_key=True, autoincrement=True);
-nome = Colum("nome", String);
-email = Colum("email", String, nullable=False);
-senha = Colum("senha", String);
-ativo = Colum("ativo", Boolean);
-admin = Colum("admin", Boolean, default=False);
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    nome = Column("nome", String)
+    email = Column("email", String, nullable=False)
+    senha = Column("senha", String)
+    ativo = Column("ativo", Boolean)
+    admin = Column("admin", Boolean, default=False)
 
-def __init__(self, nome, email, senha, ativo = True, admin = False ): 
-    self.nome = nome
-    self.email = email
-    self.senha = senha
-    self.ativo = ativo
-    self.admin = admin
+    def __init__(self, nome, email, senha, ativo=True, admin=False): 
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.ativo = ativo
+        self.admin = admin
 
 class Pedidos(Base):
-     __tablename__ = "pedidos"
+    __tablename__ = "pedidos"
 
-STATUS_PEDIDOS = (
-    ("PENDENTE", "PENDENTE"),
-    ("CANCELADO", "CANCELADO"),
-    ("FINALIZADO", "FINALIZADO")
-)
+    STATUS_PEDIDOS = (
+        ("PENDENTE", "PENDENTE"),
+        ("CANCELADO", "CANCELADO"),
+        ("FINALIZADO", "FINALIZADO")
+    )
 
-id = Colum("id", Integer, primary_key=True, autoincrement=True);
-status = Colum("status", ChoiceType(choices = STATUS_PEDIDOS));
-usuario = Colum("usuario", String, ForeignKey("usuarios.id"));
-preco = Colum("preco", Float );
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    status = Column("status", ChoiceType(choices=STATUS_PEDIDOS))
+    usuario = Column("usuario", Integer, ForeignKey("usuarios.id"))
+    preco = Column("preco", Float)
 
-def __init__(self, usuario, status = "PENDENTE", preco = 0): 
-    self.status = status
-    self.usuario = usuario
-    self.preco = preco
-
+    def __init__(self, usuario, status="PENDENTE", preco=0): 
+        self.status = status
+        self.usuario = usuario
+        self.preco = preco
 
 class ItemPedido(Base):
     __tablename__ = "pedido_itens"
 
-    id = Colum("id", Integer, primary_key=True, autoincrement=True);
-    quantidade = Colum("quantidade", Integer);
-    sabor = Colum("sabor", String);
-    tamanho = Colum("tamanho", String)
-    preco_unitario = Colum("preco_unitario", Float);
-    pedido = Colum("pedido", Integer, ForeignKey("pedidos.id"));
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    quantidade = Column("quantidade", Integer)
+    sabor = Column("sabor", String)
+    tamanho = Column("tamanho", String)
+    preco_unitario = Column("preco_unitario", Float)
+    pedido = Column("pedido", Integer, ForeignKey("pedidos.id"))
 
-def __init__(self, quantidade, sabor, tamanho, preco_unitario, pedido): 
-    self.quantidade = quantidade
-    self.sabor = sabor
-    self.tamanho = tamanho
-    self.preco_unitario = preco_unitario
-    self.pedido = pedido
-    
+    def __init__(self, quantidade, sabor, tamanho, preco_unitario, pedido): 
+        self.quantidade = quantidade
+        self.sabor = sabor
+        self.tamanho = tamanho
+        self.preco_unitario = preco_unitario
+        self.pedido = pedido
