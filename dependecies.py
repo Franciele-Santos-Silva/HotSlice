5e2e24871555ed76d09ebfabbbcf67d20f104b2c
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-11-10 13:15:15 -0300

    bcrypt

--- dependecies.py
+++ dependecies.py
@@ -2,7 +2,9 @@ from models import db
 from sqlalchemy.orm import sessionmaker
 
 def pegar_sessao():
-    Session = sessionmaker(bind=db)
-    session = Session()
-
-    return session
+    try:
+        Session = sessionmaker(bind=db)
+        session = Session()
+        yield session
+    finally:
+        session.close()
